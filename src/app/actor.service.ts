e3da7ec3a2917e6ba8d11b23d60f8b4117f0699e
kaushikrk <r.kaushik1802@gmail.com>
2018-08-09 23:05:28 +0530

    changes for registration form

--- src/app/actor.service.ts
+++ src/app/actor.service.ts
@@ -21,8 +21,8 @@ export class ActorService {
 
     }
     getActorsList(): Observable<any> {
-    // return this.http.get('https://2e49lds24e.execute-api.ap-south-1.amazonaws.com/prod/getAllActors');
-    return this.http.get('../assets/actors.json');
+    return this.http.get('https://2e49lds24e.execute-api.ap-south-1.amazonaws.com/prod/getAllActors');
+    // return this.http.get('../assets/actors.json');
     }
     saveActor(data): Observable<any>{
         return this.http.post('https://2e49lds24e.execute-api.ap-south-1.amazonaws.com/prod/saveProfile',data);
